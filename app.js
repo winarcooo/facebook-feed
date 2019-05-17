bd537a41f55cddef7374d297961b85606aa0005f
Andy Winarko <winarcooo@gmail.com>
2019-05-18 03:15:06 +0700

    port to uppercase

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
