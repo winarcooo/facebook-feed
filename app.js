d1117e82af6da529a1c07084bfd6eccfa2f40a01
Andy Winarko <winarcooo@gmail.com>
2019-05-18 03:16:56 +0700

    update again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
